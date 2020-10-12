7a65f7210a840fba04441b9160491dd56b1d8d00
gavinev98 <gavin.everett48@hotmail.com>
2020-10-12 15:19:20 +0100

    setting up some basic stores for reducer method.

--- src/redux-basics.js
+++ src/redux-basics.js
@@ -10,18 +10,22 @@ const initialState = {
 
 //reducer takes current state and action and returns updated state.
 const rootReducer = (state = initialState, action) => {
-    
+    //we retrieve the actions as a second argument.
+
+
     return state;
 
 };
 
 //store needs to be initialized with a reducer.
 const store = createStore(rootReducer);
+
+
+
+
+//action / dispatching action each action must have a type.
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-
-
-
-//action / dispatching action
-
 
 //subscription 
