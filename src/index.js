9b7dbccb66827f504e649f6ada849656618de760
gavinev98 <gavin.everett48@hotmail.com>
2020-10-13 12:15:57 +0100

    added store to provider helper component.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
+import { Provider } from 'react-redux';
 
 
 const store = createStore(reducer);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
