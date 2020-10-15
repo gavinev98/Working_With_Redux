50d1ddf69111e99ecafe52bf209656e1009b186a
gavinev98 <gavin.everett48@hotmail.com>
2020-10-15 14:17:52 +0100

    refactored to switch statement from IF.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,35 +9,29 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
-    //check if action type is INCREMENT.
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
+    switch(action.type){
+
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        break;
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        break;
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        break;
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.val
+            }
+        break;     
     }
-
-    //check if action type is DECREMENT.
-    if(action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-
-    //check if action type is ADDITION
-    if(action.type === 'ADD') {
-        return {
-            counter: state.counter + action.val
-        }
-    }
-
-    //check if action type is SUBTRACT
-    if(action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.val
-        }
-    }
-
-
 
     return state;
 }
