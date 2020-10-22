ea46c122ed267b3d4fcfab3c8d8637e4708330fe
gavinev98 <gavin.everett48@hotmail.com>
2020-10-22 11:01:06 +0100

    setting up a middleware function

--- src/index.js
+++ src/index.js
@@ -16,6 +16,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
+//simple middleware which logs each action
+const logger = store => {
+    //this function will recieve the next argument eg pass to reducer
+    return next => {
+        //this function return a function will recieve an action you dispatch
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            //the next method will allow action to continue to reducer method.
+            const result = next(action);
+            return result;
+        }
+    }
+}
+
 const store = createStore(rootReducer);
 
 
