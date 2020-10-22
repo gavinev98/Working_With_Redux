00a0a9288216f9dfa0746eaa177cd27d96e815d0
gavinev98 <gavin.everett48@hotmail.com>
2020-10-22 11:07:58 +0100

    applied middleware to our store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
 import counterReducer from './store/reducers/counter';
