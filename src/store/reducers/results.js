bc501ccd04212f64bfde49a46794e7aab8293e93
gavinev98 <gavin.everett48@hotmail.com>
2020-10-25 15:15:18 +0000

    added helper function to make reducer lean.

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -8,19 +8,22 @@ const initialState = {
 
 }
 
+const deleteResult = (state, action) => {
+
+    const updatedArray = state.results.filter(result => result.id !== action.resID);
+    return updateObject(state, {results: updatedArray});  
+}
 
 const reducer = (state = initialState, action) => {
 
     switch(action.type){
-
         case actionTypes.STORE_RESULT: 
         return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
         break;
         case actionTypes.REMOVE_RESULT:
             //accquire index
             //create copy of the array. filter creates copy and executes function on each element in array.
-            const updatedArray = state.results.filter(result => result.id !== action.resID);
-            return updateObject(state, {results: updatedArray});            
+            return deleteResult(state, action);    
     }
     return state;
 }
